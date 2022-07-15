7e162c7e0a7b93a565df4ab96e11f8af8005c183
Yurema <yuremix.b.s@gmail.com>
2022-07-14 19:34:16 -0500

    m

--- src/data.js
+++ src/data.js
@@ -1,24 +1,93 @@
-export const Especies
-const app = {
-  SelectPersonajes: 0,
+export {}
 
-  iniciar: function () {
-    console.log("iniciamos")
-  },
-  buscador: function (element) {
-    document.getElementById("buscador").addEventListener("click", this.handleBuscador)
-  },
-  handleBuscador: function (event) {
-    console.log(event)
-  }
-}
 
-export default app
 
-// para el buscador
 
-// para el desplegable de los personajes
+// manipulación con arreglos
+import data from './data/rickandmorty/rickandmorty.js';
+let buscador = [i]
+data.filter ((elemento, index, array) => {
+console.log(index)
+})
 
-// para el desplegable de las especies
 
-// para el desplegable de los episodios
+// console.log(personajes.filter(personaje => ["J","H", "P"].includes(personaje.name.charAt(0))).sort());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // export const Especies
+
+// // const  = {
+// //   SelectPersonajes: 0,
+
+// //   iniciar: function () {
+// //     console.log("iniciamos")
+// //   },
+// //   buscador: function (element) {
+// //     document.getElementById("buscador").addEventListener("click", this.handleBuscador)
+// //   },
+// //   handleBuscador: function (event) {
+// //     console.log(event)
+// //   }
+// // }
+
+// // export default app
+
+// // para el buscador
+
+// // para el desplegable de los personajes
+// export const filterPersonajes = (datos, gato) => {
+//   return  datos.filter(element => element.species); 
+
+// };
+
+
+
+// // para el desplegable de las especies
+
+// export const filterEspecies = (datos, gato) => {
+//   return  datos.filter(element => element.species == gato); 
+
+// };
+
+// // para el desplegable de los episodios
+// export const filterEsp = (datos, gato) => {
+//   return  datos.filter(element => element.species == gato); 
+
+// };
+// //     if(parametro.includes(e.name[0].toLowerCase())){
+//         arreglo.push(e)
--- src/main.js
+++ src/main.js
@@ -1,19 +1,125 @@
-import data from './data/rickandmorty/rickandmorty.js';
-import app from './data.js';
+ppp-pppimport data from './data/rickandmorty/rickandmorty.js';
+const personajes = data.results;
+// para el buscador
+// para las personajes
+// para lso episodios
+// para las especies
+// const especies = data.results.filter(p => .name == );
+// console.log(especies)
 
-const buscador = document.getElementById("buscador");
-const selectPersonajes = document.getElementById("selectPersonajes");
-const selectEpisodios = document.getElementById("selectEpisodios");
-const selectEspecie = document.getElementById("selectEspecie");
 
-app.inciar()
 
-app.buscador(buscador)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// array de todos los personajes
+// const api_url = data.results ;
+p´
+// const buscador = document.getElementById("buscador")
+// buscador.innerHTML = `<p> ${data.name} </p>` 
+// // const content = document.getElementById("content")
+// const selectPersonajes = document.getElementById("selectPersonajes")
 
 // para el buscador
 
-// para el desplegable de los personajes
 
-// para el desplegable de las especies
+// // // para el desplegable de los PERSONAJES
+// import {filterPersonajes} from './data.js';
 
-// para el desplegable de los episodios
+// const personajes = personajes.filter (personas => personas.name = j);
+// console.log(personajes)
+
+//      // Creamos una funcion que muestre los personajes
+//      function mostrarPersonajes(personajes){
+//         personajes.forEach(element => {  
+//         const nodeEspecie = document.createElement("div");
+//                 // nodeEspecie.innerHTML=personajes[i].name
+//                 nodeEspecie.innerHTML =`<img src="${element.image}"/> <p>${element.name}</p>`;
+//                 todoshumanos.appendChild(nodeEspecie)
+//         });
+//         }
+
+// // para el desplegable de las especies
+// import { filterEspecies } from './data.js';
+//     //select por especies
+//         const selectEspecie = document.querySelector("#selectEspecie")
+//         selectEspecie.addEventListener("change", (event) => {   
+//         todoshumanos.innerHTML="";
+//         // te entrega el valor que seleccionaste
+//         const valueSelect=  event.target.value;
+
+//         mostrarPersonajes(filterEspecies(personajes,valueSelect));
+
+//         })    
+
+// // para el desplegable de los episodios
+// import {filterEpisodios } from './data.js';
+
+
+
+
+
+
+
+
+
+
+// const personajes = data.results
+
+// // change nos regala informacion
+// // cuando sucede algo
+// // y es gratis.......
+// // te la manda en forma de regalito, como argumento!!!!
+// // el regalo es :ooooooo
+// // la informacion de lo que suecede en el evento
+// selectPersonajes.addEventListener("change", function (event) {
+//     const opcionSeleccionada = event.target.value
+
+//     if (opcionSeleccionada == "ah") {
+
+//         content.innerHTML = ""
+
+//         // para recorrer arrays
+//         personajes.forEach(function (personaje){
+            
+//             //personaje.name
+
+//             let html = `<div><h3>${personaje.name}</h3><img src="${personaje.image}" /></div>`
+//             content.insertAdjacentHTML("beforeend", html)
+//         })
+//     }
+
+// })
+
